317138d7dc24c0e932d8e63e057e62892c2413d1
cclabdis <cclabdis@gmail.com>
2023-03-02 16:08:40 -0300

    exercicio 2 iac2 bootcamp linux

---
+++ iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Atualizando server . . ."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+
+
+echo "Baixando e copiando arquivos para a aplicação . . ."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
